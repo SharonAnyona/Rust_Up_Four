e8c303b964a41fcc8fa9e23af4cc11416ccb15b7
Ephraimnorbat <ephraimnorbat@gmail.com>
2024-01-21 17:07:02 +0300

    Traditional hello world program

---
+++ week1/Ephraim/target/debug/build/serde-617751425e1ad088/build_script_build-617751425e1ad088.d
@@ -0,0 +1,5 @@
+/home/phrazy/Rust_Up_Four/week1/Ephraim/target/debug/build/serde-617751425e1ad088/build_script_build-617751425e1ad088: /home/phrazy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.195/build.rs
+
+/home/phrazy/Rust_Up_Four/week1/Ephraim/target/debug/build/serde-617751425e1ad088/build_script_build-617751425e1ad088.d: /home/phrazy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.195/build.rs
+
+/home/phrazy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.195/build.rs:

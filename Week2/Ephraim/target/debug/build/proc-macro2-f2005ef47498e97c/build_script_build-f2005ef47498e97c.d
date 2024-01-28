55d41fa1128eeb1d5f0d37deed14f3a6d886e3c0
Ephraimnorbat <ephraimnorbat@gmail.com>
2024-01-28 20:11:47 +0300

    Basic function in rust

---
+++ Week2/Ephraim/target/debug/build/proc-macro2-f2005ef47498e97c/build_script_build-f2005ef47498e97c.d
@@ -0,0 +1,5 @@
+/home/phrazy/Rust_Up_Four/Week2/Ephraim/target/debug/build/proc-macro2-f2005ef47498e97c/build_script_build-f2005ef47498e97c: /home/phrazy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.78/build.rs
+
+/home/phrazy/Rust_Up_Four/Week2/Ephraim/target/debug/build/proc-macro2-f2005ef47498e97c/build_script_build-f2005ef47498e97c.d: /home/phrazy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.78/build.rs
+
+/home/phrazy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.78/build.rs:
---
+++ Week2/Ephraim/target/debug/build/serde-5d46b6ce2f1ef9e6/build_script_build-5d46b6ce2f1ef9e6.d
@@ -0,0 +1,5 @@
+/home/phrazy/Rust_Up_Four/Week2/Ephraim/target/debug/build/serde-5d46b6ce2f1ef9e6/build_script_build-5d46b6ce2f1ef9e6: /home/phrazy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.196/build.rs
+
+/home/phrazy/Rust_Up_Four/Week2/Ephraim/target/debug/build/serde-5d46b6ce2f1ef9e6/build_script_build-5d46b6ce2f1ef9e6.d: /home/phrazy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.196/build.rs
+
+/home/phrazy/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.196/build.rs:
